d689b99cb8223e39bbc4509e4ef03fb44a5387da
sloanstewart <sloan_stewart@hotmail.com>
2017-06-28 14:31:06 -0400

    intial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var todoController = require('./controllers/todo-controller');
+// set up template engine
+app.set('view engine', 'ejs');
+
+// serve static files from "/public" directory
+app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen(80);
+console.log('Listening on port 80');
---
+++ controllers/todo-controller.js
@@ -0,0 +1,41 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to database
+mongoose.connect('mongodb://test:test@ds139362.mlab.com:39362/node-todo-test');
+
+// create schema and model for mongoDB
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+var Todo = mongoose.model('Todo', todoSchema);
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function(app){
+
+  app.get('/todo', function(req, res){
+    // get data from mongoDB and pass it to the view
+    Todo.find({}, function(err, data){
+      if(err) throw err;
+      res.render('todo', {todos: data});
+    });
+  });
+
+  app.post('/todo', urlencodedParser, function(req, res){
+    // get data from view and add to mongoDB
+    var newTodo = Todo(req.body).save(function(err, data){
+      if(err) throw err;
+      res.json(data);
+    })
+  });
+
+  app.delete('/todo/:item', function(req, res){
+    //delete item from database
+    Todo.find({item: req.params.item.replace(/\-/g, ' ')}).remove(function(err, data){
+      if(err) throw err;
+      res.json(data);
+    })
+  });
+
+};
